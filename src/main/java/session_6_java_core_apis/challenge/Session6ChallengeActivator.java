6eb7926feddea26d7a453a3c5a7720ddf968829f
Sabin <sabins.badea@gmail.com>
2024-07-22 20:22:53 +0300

    correct List variable declaration

--- src/main/java/session_6_java_core_apis/challenge/Session6ChallengeActivator.java
+++ src/main/java/session_6_java_core_apis/challenge/Session6ChallengeActivator.java
@@ -1,5 +1,6 @@
 package session_6_java_core_apis.challenge;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,12 +15,12 @@ public class Session6ChallengeActivator {
         /*Challenge2*/
         System.out.println("O fi palindrom? " + Challenge2.isPalindrome(string));
         /*Challenge3*/
-        List<String> stringlist = new List<String>();
+        List<String> stringlist = new ArrayList<String>();
         stringlist.add("11fgr1");
         stringlist.add("222sdf2");
         stringlist.add("33333");
         stringlist.add("4a444g44");
         stringlist.add("5t555");
-        System.out.println("Concatenated: "+Challege4.concat());
+        System.out.println("Concatenated: "+Challege4.concat(stringlist));
     }
 }
